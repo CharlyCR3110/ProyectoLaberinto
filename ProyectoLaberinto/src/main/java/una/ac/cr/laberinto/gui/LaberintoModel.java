686a501f48e161eb39720ccfd0861f6f5fb947fe
charly <carlosgarita3110@gmail.com>
2023-11-04 23:30:41 -0600

    feat: add algorithm to solve maze and allow player to solve maze with spacebar

--- ProyectoLaberinto/src/main/java/una/ac/cr/laberinto/gui/LaberintoModel.java
+++ ProyectoLaberinto/src/main/java/una/ac/cr/laberinto/gui/LaberintoModel.java
@@ -1,17 +1,21 @@
 package una.ac.cr.laberinto.gui;
 
+import una.ac.cr.laberinto.algoritmos.AlgoritmoSolucion;
 import una.ac.cr.laberinto.modelo.Laberinto;
 import una.ac.cr.laberinto.modelo.Nodo;
 
 import java.awt.event.KeyEvent;
+import java.util.List;
 
 public class LaberintoModel {
 	private Laberinto laberinto;
+	private AlgoritmoSolucion algoritmoSolucion;
 	private Nodo jugador; // Representa la posición del jugador en el laberinto
 	private LaberintoPanel panel;
 	public LaberintoModel(Laberinto laberinto, LaberintoPanel panel) {
 		this.laberinto = laberinto;
 		this.panel = panel;
+		this.algoritmoSolucion = new AlgoritmoSolucion(laberinto);
 		this.jugador = laberinto.getNodoEntrada(); // El jugador comienza en el nodo de entrada
 	}
 
@@ -22,36 +26,41 @@ public class LaberintoModel {
 	public void moverJugador(KeyEvent keyEvent) {
 		if (keyEvent.getKeyCode() == KeyEvent.VK_UP) {
 			if (jugador.getArriba() != null) {
-				jugador.marcarVisitado();
 				jugador = jugador.getArriba();
+				jugador.marcarVisitado();
 			} else {
 				System.out.println("No se puede mover hacia arriba");
 				return;
 			}
 		} else if (keyEvent.getKeyCode() == KeyEvent.VK_DOWN) {
 			if (jugador.getAbajo() != null) {
-				jugador.marcarVisitado();
 				jugador = jugador.getAbajo();
+				jugador.marcarVisitado();
 			} else {
 				System.out.println("No se puede mover hacia abajo");
 				return;
 			}
 		} else if (keyEvent.getKeyCode() == KeyEvent.VK_LEFT) {
 			if (jugador.getIzquierda() != null) {
-				jugador.marcarVisitado();
 				jugador = jugador.getIzquierda();
+				jugador.marcarVisitado();
 			} else {
 				System.out.println("No se puede mover hacia la izquierda");
 				return;
 			}
 		} else if (keyEvent.getKeyCode() == KeyEvent.VK_RIGHT) {
 			if (jugador.getDerecha() != null) {
-				jugador.marcarVisitado();
 				jugador = jugador.getDerecha();
+				jugador.marcarVisitado();
 			} else {
 				System.out.println("No se puede mover hacia la derecha");
 				return;
 			}
+		} else if(keyEvent.getKeyCode() == KeyEvent.VK_SPACE) {
+			System.out.println("Resolviendo laberinto");
+			algoritmoSolucion.resolverLaberinto();
+			jugador = laberinto.getNodoSalida();
+			return;
 		} else {
 			System.out.println("Tecla no válida");
 			return;
