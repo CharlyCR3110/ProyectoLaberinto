f7e3d9d3c620abcbde83f0f53fda6c0572842f9b
charly <carlosgarita3110@gmail.com>
2023-11-04 20:04:48 -0600

    feat: add directional nodes to Nodo class

--- ProyectoLaberinto/src/main/java/una/ac/cr/laberinto/modelo/Nodo.java
+++ ProyectoLaberinto/src/main/java/una/ac/cr/laberinto/modelo/Nodo.java
@@ -5,6 +5,11 @@ public class Nodo {
 	private int columna;
 	private boolean visitado;
 
+	private Nodo arriba;
+	private Nodo derecha;
+	private Nodo abajo;
+	private Nodo izquierda;
+
 	public Nodo(int fila, int columna) {
 		this.fila = fila;
 		this.columna = columna;
@@ -31,5 +36,38 @@ public class Nodo {
 	public String toString() {
 		return "(" + fila + ", " + columna + ")";
 	}
+
+	public void setDerecha(Nodo nodo) {
+		this.derecha = nodo;
+	}
+
+	public void setIzquierda(Nodo nodo) {
+		this.izquierda = nodo;
+	}
+
+	public void setArriba(Nodo nodo) {
+		this.arriba = nodo;
+	}
+
+	public void setAbajo(Nodo nodo) {
+		this.abajo = nodo;
+	}
+
+	public Nodo getArriba() {
+		return arriba;
+	}
+
+	public Nodo getDerecha() {
+		return derecha;
+	}
+
+	public Nodo getAbajo() {
+		return abajo;
+	}
+
+	public Nodo getIzquierda() {
+		return izquierda;
+	}
+
 }
 
