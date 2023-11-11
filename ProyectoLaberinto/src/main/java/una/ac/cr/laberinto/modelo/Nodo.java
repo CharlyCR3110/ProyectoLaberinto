b8cb1dbf446a4099a316794ca7d98fe2af29c5f3
charly <carlosgarita3110@gmail.com>
2023-11-11 10:50:28 -0600

    feat: add XML annotations and ID fields to Nodo class

--- ProyectoLaberinto/src/main/java/una/ac/cr/laberinto/modelo/Nodo.java
+++ ProyectoLaberinto/src/main/java/una/ac/cr/laberinto/modelo/Nodo.java
@@ -1,20 +1,37 @@
 package una.ac.cr.laberinto.modelo;
 
+import jakarta.xml.bind.annotation.*;
+
+@XmlAccessorType(XmlAccessType.FIELD)
 public class Nodo {
 	private int fila;
 	private int columna;
 	private boolean visitado;
 	private boolean caminoIncorrecto;
-
+	@XmlID
+	private String id;
+	@XmlIDREF
+	@XmlElement
 	private Nodo arriba;
+	@XmlIDREF
+	@XmlElement
 	private Nodo derecha;
+	@XmlIDREF
+	@XmlElement
 	private Nodo abajo;
+	@XmlIDREF
+	@XmlElement
 	private Nodo izquierda;
 
 	public Nodo(int fila, int columna) {
 		this.fila = fila;
 		this.columna = columna;
 		this.visitado = false;
+		this.id = "Nodo: " + fila + "," + columna;
+	}
+
+	public Nodo() {
+		this(0, 0);
 	}
 
 	public int getFila() {
