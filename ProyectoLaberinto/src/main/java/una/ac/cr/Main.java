ed416ce1630aad525173c1f4fa1774ea9a8095da
charly <carlosgarita3110@gmail.com>
2023-11-18 10:14:59 -0600

    refactor: add MainFrame and update main method to use it

--- ProyectoLaberinto/src/main/java/una/ac/cr/Main.java
+++ ProyectoLaberinto/src/main/java/una/ac/cr/Main.java
@@ -2,21 +2,17 @@ package una.ac.cr;
 
 import una.ac.cr.laberinto.algoritmos.AlgoritmoGeneracion;
 import una.ac.cr.laberinto.gui.laberinto.LaberintoFrame;
+import una.ac.cr.laberinto.gui.main.MainFrame;
 import una.ac.cr.laberinto.modelo.Laberinto;
 
 import javax.swing.*;
 
 public class Main {
 	public static void main(String[] args) {
-		int filas = 10; // Define el número de filas de tu laberinto
-		int columnas = 10; // Define el número de columnas de tu laberinto
-		AlgoritmoGeneracion algoritmoGeneracion = new AlgoritmoGeneracion(filas, columnas);
-		Laberinto laberinto = algoritmoGeneracion.getLaberinto();
-
-		laberinto.mostrarNodos();
-
 		SwingUtilities.invokeLater(() -> {
-			new LaberintoFrame(laberinto);
+			// Crea la ventana principal
+			MainFrame mainFrame = new MainFrame();
+			mainFrame.setVisible(true);
 		});
 	}
 }
