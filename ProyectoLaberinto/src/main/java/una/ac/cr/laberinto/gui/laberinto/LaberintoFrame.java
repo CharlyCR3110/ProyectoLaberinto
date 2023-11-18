e3f2ef37efbfb7148d1bb74b5fec717666dec335
charly <carlosgarita3110@gmail.com>
2023-11-18 14:27:58 -0600

    chore: remove "Guardar" button from LaberintoFrame

--- ProyectoLaberinto/src/main/java/una/ac/cr/laberinto/gui/laberinto/LaberintoFrame.java
+++ ProyectoLaberinto/src/main/java/una/ac/cr/laberinto/gui/laberinto/LaberintoFrame.java
@@ -65,14 +65,6 @@ public class LaberintoFrame extends JFrame {
 			}
 		});
 
-		d.add(new JButton("Guardar") {	// guarda el laberinto en un archivo
-			{
-				addActionListener((ActionEvent e) -> {
-					guardar();
-				});
-			}
-		});
-
 		c.add(BorderLayout.PAGE_START, d);
 		c.add(BorderLayout.CENTER,
 				new JScrollPane(
@@ -87,35 +79,6 @@ public class LaberintoFrame extends JFrame {
 //			ajustarModo();
 //		});
 	}
-	private void guardar() {
-		laberintoPanel.getController().reiniciar();
-		laberintoPanel.repaint();
-		String nombre = JOptionPane.showInputDialog("Nombre del laberinto");
-
-		if (!esNombreValido(nombre)) { return; }
-
-		if (JAXBUtil.guardarLaberinto(laberinto, nombre) == 1) {
-			JOptionPane.showMessageDialog(this, "Laberinto guardado correctamente");
-		} else {
-			JOptionPane.showMessageDialog(this, "Error al guardar el laberinto");
-		}
-	}
-
-	private boolean esNombreValido(String nombre) {
-		if (nombre == null || nombre.isEmpty()) {
-			JOptionPane.showMessageDialog(this, "El nombre del laberinto no puede estar vacío");
-			return false;
-		}  else if (!PathChecker.isNameValid(nombre)) {
-			JOptionPane.showMessageDialog(this, "El nombre del laberinto no es válido \n" +
-					"El nombre solo puede contener letras y números");
-			return false;
-		} else if (!PathChecker.isValid(nombre)) {
-			JOptionPane.showMessageDialog(this, "Parece que ya existe un laberinto con ese nombre");
-			return false;
-		}
-		return true;
-	}
-
 
 	private void handleLaberintoFrameClosing() {
 		LaberintoInfo laberintoInfo = new LaberintoInfo(laberinto);
