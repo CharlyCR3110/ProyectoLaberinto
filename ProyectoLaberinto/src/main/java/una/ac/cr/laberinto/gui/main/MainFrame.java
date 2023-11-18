f5af4d893a4f0eac2ebbe698d9a2762e549ce722
charly <carlosgarita3110@gmail.com>
2023-11-18 10:50:39 -0600

    refactor: enhace MainFrame class to improve code structure and readability

--- ProyectoLaberinto/src/main/java/una/ac/cr/laberinto/gui/main/MainFrame.java
+++ ProyectoLaberinto/src/main/java/una/ac/cr/laberinto/gui/main/MainFrame.java
@@ -12,96 +12,112 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class MainFrame extends JFrame {
 
 	private MainFrameController controller;
 
 	public MainFrame(MainFrameController controller) {
 		this.controller = controller;
-		// Configura la ventana principal
+		initializeUI();
+	}
+
+	private void initializeUI() {
 		setTitle("Laberinto App");
 		setSize(400, 400);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-		// Crea un panel para contener los componentes
 		JPanel mainPanel = new JPanel(new BorderLayout());
 
-		// Crea un botón para generar un nuevo laberinto
+		JButton nuevoLaberintoButton = createNuevoLaberintoButton();
+		JButton recuperarLaberintoButton = createRecuperarLaberintoButton();
+
+		JPanel buttonPanel = createButtonPanel(nuevoLaberintoButton, recuperarLaberintoButton);
+
+		mainPanel.add(buttonPanel, BorderLayout.NORTH);
+
+		add(mainPanel);
+	}
+
+	private JButton createNuevoLaberintoButton() {
 		JButton nuevoLaberintoButton = new JButton("Nuevo Laberinto");
 		nuevoLaberintoButton.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				JPanel dimensionPanel = new JPanel(new GridLayout(2, 2));
-				JLabel filasLabel = new JLabel("Filas:");
-				JLabel columnasLabel = new JLabel("Columnas:");
-				JTextField filasField = new JTextField();
-				JTextField columnasField = new JTextField();
-
-				dimensionPanel.add(filasLabel);
-				dimensionPanel.add(filasField);
-				dimensionPanel.add(columnasLabel);
-				dimensionPanel.add(columnasField);
-
-				// Muestra el cuadro de dialogo para obtener las dimensiones del laberinto
-				int result = JOptionPane.showConfirmDialog(MainFrame.this, dimensionPanel, "Nuevo Laberinto",
-						JOptionPane.OK_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE);
-
-				// Revisa el resultado del cuadro de diálogo y crea un nuevo laberinto (si es posible)
-				if (result == JOptionPane.OK_OPTION) {
-					try {
-						int filas = Integer.parseInt(filasField.getText().trim());
-						int columnas = Integer.parseInt(columnasField.getText().trim());
-
-						if (filas < 4 || columnas < 4) {
-							JOptionPane.showMessageDialog(MainFrame.this, "Por favor, introduce números mayores o iguales a 4.", "Error", JOptionPane.ERROR_MESSAGE);
-							return;
-						}
-
-						AlgoritmoGeneracion algoritmoGeneracion = new AlgoritmoGeneracion(filas, columnas);
-						Laberinto laberinto = algoritmoGeneracion.getLaberinto();
-
-						// Crea y muestra la nueva ventana del laberinto
-						LaberintoFrame laberintoFrame = new LaberintoFrame(laberinto);
-						laberintoFrame.setVisible(true);
-					} catch (NumberFormatException ex) {
-						JOptionPane.showMessageDialog(MainFrame.this, "Por favor, introduce números válidos.", "Error", JOptionPane.ERROR_MESSAGE);
-					}
-				}
+				handleNuevoLaberintoButtonClick();
 			}
 		});
+		return nuevoLaberintoButton;
+	}
 
-		// Crea un botón para recuperar un laberinto desde un archivo
+	private JButton createRecuperarLaberintoButton() {
 		JButton recuperarLaberintoButton = new JButton("Recuperar Laberinto");
 		recuperarLaberintoButton.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				// Se llama al método recuperar() para recuperar el laberinto desde un XML
-				Laberinto laberinto = controller.recuperar();
-				if (laberinto != null) {
-					// Crea y muestra la nueva ventana del laberinto
-					LaberintoFrame laberintoFrame = new LaberintoFrame(laberinto);
-					laberintoFrame.setVisible(true);
-				} else {
-					JOptionPane.showMessageDialog(MainFrame.this, "No se recuperó ningún laberinto", "Informacion", JOptionPane.INFORMATION_MESSAGE);
-				}
+				handleRecuperarLaberintoButtonClick();
 			}
 		});
+		return recuperarLaberintoButton;
+	}
 
-
-		// Crea un panel para contener los botones
+	private JPanel createButtonPanel(JButton nuevoLaberintoButton, JButton recuperarLaberintoButton) {
 		JPanel buttonPanel = new JPanel(new FlowLayout());
 		buttonPanel.add(nuevoLaberintoButton);
 		buttonPanel.add(recuperarLaberintoButton);
-
-		// Agrega el panel de botones al panel principal en la parte superior
-		mainPanel.add(buttonPanel, BorderLayout.NORTH);
-
-//		// Agrega la tabla al centro del panel principal
-//		mainPanel.add(scrollPane, BorderLayout.CENTER);
-
-		// Agrega el panel principal a la ventana
-		add(mainPanel);
+		return buttonPanel;
 	}
 
+	private void handleNuevoLaberintoButtonClick() {
+		JPanel dimensionPanel = new JPanel(new GridLayout(2, 2));
+		JLabel filasLabel = new JLabel("Filas:");
+		JLabel columnasLabel = new JLabel("Columnas:");
+		JTextField filasField = new JTextField();
+		JTextField columnasField = new JTextField();
 
+		dimensionPanel.add(filasLabel);
+		dimensionPanel.add(filasField);
+		dimensionPanel.add(columnasLabel);
+		dimensionPanel.add(columnasField);
+
+		// Muestra el cuadro de dialogo para obtener las dimensiones del laberinto
+		int result = JOptionPane.showConfirmDialog(MainFrame.this, dimensionPanel, "Nuevo Laberinto",
+				JOptionPane.OK_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE);
+
+		// Revisa el resultado del cuadro de diálogo y crea un nuevo laberinto (si es posible)
+		if (result == JOptionPane.OK_OPTION) {
+			try {
+				int filas = Integer.parseInt(filasField.getText().trim());
+				int columnas = Integer.parseInt(columnasField.getText().trim());
+
+				if (filas < 4 || columnas < 4) {
+					JOptionPane.showMessageDialog(MainFrame.this, "Por favor, introduce números mayores o iguales a 4.", "Error", JOptionPane.ERROR_MESSAGE);
+					return;
+				}
+
+				AlgoritmoGeneracion algoritmoGeneracion = new AlgoritmoGeneracion(filas, columnas);
+				Laberinto laberinto = algoritmoGeneracion.getLaberinto();
+
+				// Crea y muestra la nueva ventana del laberinto
+				LaberintoFrame laberintoFrame = new LaberintoFrame(laberinto);
+				laberintoFrame.setVisible(true);
+			} catch (NumberFormatException ex) {
+				JOptionPane.showMessageDialog(MainFrame.this, "Por favor, introduce números válidos.", "Error", JOptionPane.ERROR_MESSAGE);
+			}
+		}
+	}
+
+	private void handleRecuperarLaberintoButtonClick() {
+		Laberinto laberinto = this.controller.recuperar();
+		if (laberinto != null) {
+			LaberintoFrame laberintoFrame = new LaberintoFrame(laberinto);
+			laberintoFrame.setVisible(true);
+		} else {
+			JOptionPane.showMessageDialog(this, "No se recuperó ningún laberinto", "Informacion", JOptionPane.INFORMATION_MESSAGE);
+		}
+	}
 }
