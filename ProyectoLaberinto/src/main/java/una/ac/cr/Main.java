9c37395144e2eb5807228d42608a6f41ef94e7a4
charly <carlosgarita3110@gmail.com>
2023-11-18 10:48:15 -0600

    refactor: add MainFrameController to Main.java

--- ProyectoLaberinto/src/main/java/una/ac/cr/Main.java
+++ ProyectoLaberinto/src/main/java/una/ac/cr/Main.java
@@ -3,15 +3,16 @@ package una.ac.cr;
 import una.ac.cr.laberinto.algoritmos.AlgoritmoGeneracion;
 import una.ac.cr.laberinto.gui.laberinto.LaberintoFrame;
 import una.ac.cr.laberinto.gui.main.MainFrame;
+import una.ac.cr.laberinto.gui.main.MainFrameController;
 import una.ac.cr.laberinto.modelo.Laberinto;
 
 import javax.swing.*;
 
 public class Main {
 	public static void main(String[] args) {
+		MainFrameController mainFrameController = new MainFrameController();
 		SwingUtilities.invokeLater(() -> {
-			// Crea la ventana principal
-			MainFrame mainFrame = new MainFrame();
+			MainFrame mainFrame = mainFrameController.getMainFrame();
 			mainFrame.setVisible(true);
 		});
 	}
